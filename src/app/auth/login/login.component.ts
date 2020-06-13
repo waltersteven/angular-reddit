a464ef1555689a4e600e8e9421ea43d744a3c25a
Walter Pariona <wltrst97@gmail.com>
2020-06-13 12:05:46 -0500

    Added toaster component and business logic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { LoginRequestPayload } from './login-request.payload';
 import { AuthService } from '../shared/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +15,13 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   loginRequestPayload: LoginRequestPayload;
+  registerSuccessMessage: string;
+  isError: boolean;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService) {
     this.loginRequestPayload = {
       username: '',
       password: ''
@@ -24,7 +32,16 @@ export class LoginComponent implements OnInit {
     this.loginForm = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
-    })
+    });
+
+    this.activatedRoute.queryParams
+      .subscribe(params => {
+        if (params.registered !== undefined && params.registered === 'true') {
+          this.toastr.success('Signup Successful');
+          this.registerSuccessMessage = 'Please check your inbox for activation email. ' +
+            'Activate your account before you login.';
+        }
+      })
   }
 
   login() {
@@ -32,8 +49,12 @@ export class LoginComponent implements OnInit {
     this.loginRequestPayload.password = this.loginForm.get('password').value;
 
     this.authService.login(this.loginRequestPayload).subscribe(data => {
-      console.log('Login Successful');
+      this.isError = false;
+      this.router.navigateByUrl('/');
+      this.toastr.success('Login successful');
+    }, error => {
+      this.isError = true;
+      throwError(error);
     });
   }
-
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { SignupRequestPayload } from './signup-request.payload';
 import { AuthService } from '../shared/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-signup',
@@ -13,7 +15,9 @@ export class SignupComponent implements OnInit {
   signupRequestPayload: SignupRequestPayload;
   signupForm: FormGroup;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService) {
     this.signupRequestPayload = {
       username: '',
       email: '',
@@ -35,8 +39,11 @@ export class SignupComponent implements OnInit {
     this.signupRequestPayload.password = this.signupForm.get('password').value;
 
     this.authService.signup(this.signupRequestPayload)
-      .subscribe(data => {
-        console.log(data)
-      })
+      .subscribe(() => {
+        this.router.navigate(['/login'],
+          { queryParams: { registered: 'true' } });
+      }, () => {
+        this.toastr.error('Registration Failed! Please try again.');
+      });
   }
 }
