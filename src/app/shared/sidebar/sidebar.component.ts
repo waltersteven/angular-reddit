992d29399596b59a2256e5ee2e36d4698889d5eb
Walter Pariona <wltrst97@gmail.com>
2020-06-14 13:36:39 -0500

    Changed sidebar component to add click methods

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  goToCreatePost() {
+    this.router.navigateByUrl('/create-post');
+  }
+
+  goToCreateSubreddit() {
+    this.router.navigateByUrl('/create-subreddit');
+  }
 }
