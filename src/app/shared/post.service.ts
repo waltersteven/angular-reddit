46ff43501106a1efbdb94949f3733f07ce957793
Walter Pariona <wltrst97@gmail.com>
2020-06-14 14:37:39 -0500

    Added create post component

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PostModel } from './post.model';
 import { Observable } from 'rxjs';
+import { CreatePostPayload } from '../post/create-post/create-post.payload';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,13 @@ export class PostService {
 
   getAllPosts(): Observable<Array<PostModel>> {
     return this.http.get<Array<PostModel>>('http://localhost:8080/api/posts');
+  }
+
+  createPost(postPayload: CreatePostPayload): Observable<any> {
+    return this.http.post('http://localhost:8080/api/posts', postPayload);
+  }
+
+  getPost(id: Number): Observable<PostModel> {
+    return this.http.get<PostModel>('http://localhost:8080/api/posts/' + id);
   }
 }
