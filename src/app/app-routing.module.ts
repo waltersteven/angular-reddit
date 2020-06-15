f9775aa142343f8871785ff7f9bd2807199d4c74
Walter Pariona <wltrst97@gmail.com>
2020-06-14 21:15:40 -0500

    Added guard to specific routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,16 +8,17 @@ import { CreatePostComponent } from './post/create-post/create-post.component';
 import { ListSubredditComponent } from './subreddit/list-subreddit/list-subreddit.component';
 import { ViewPostComponent } from './post/view-post/view-post.component';
 import { UserProfileComponent } from './auth/user-profile/user-profile.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'create-subreddit', component: CreateSubredditComponent },
-  { path: 'create-post', component: CreatePostComponent },
+  { path: 'create-subreddit', component: CreateSubredditComponent, canActivate: [AuthGuard] },
+  { path: 'create-post', component: CreatePostComponent, canActivate: [AuthGuard] },
   { path: 'list-subreddits', component: ListSubredditComponent },
   { path: 'view-post/:id', component: ViewPostComponent },
-  { path: 'user-profile/:name', component: UserProfileComponent },
+  { path: 'user-profile/:name', component: UserProfileComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
