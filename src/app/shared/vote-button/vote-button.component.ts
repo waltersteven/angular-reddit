15702df1360ef064be47308f2d1a8ca583da94fa
Walter Pariona <wltrst97@gmail.com>
2020-06-14 20:38:55 -0500

    Added vote button business logic

--- src/app/shared/vote-button/vote-button.component.ts
+++ src/app/shared/vote-button/vote-button.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PostModel } from '../post.model';
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { VotePayload } from './vote.payload';
+import { VoteService } from '../vote.service';
+import { AuthService } from 'src/app/auth/shared/auth.service';
+import { PostService } from '../post.service';
+import { ToastrService } from 'ngx-toastr';
+import { VoteType } from './vote.type';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-vote-button',
@@ -13,10 +20,50 @@ export class VoteButtonComponent implements OnInit {
   faArrowDown = faArrowDown;
 
   @Input() post: PostModel;
+  votePayload: VotePayload;
+  upvoteColor: string;
+  downvoteColor: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private voteService: VoteService,
+    private authService: AuthService,
+    private postService: PostService,
+    private toastr: ToastrService
+  ) {
+    this.votePayload = {
+      voteType: undefined,
+      postId: undefined
+    }
   }
 
+  ngOnInit(): void {
+    this.updateVoteDetails();
+  }
+
+  upvotePost() {
+    this.votePayload.voteType = VoteType.UPVOTE;
+    this.vote();
+  }
+
+  downvotePost() {
+    this.votePayload.voteType = VoteType.DOWNVOTE;
+    this.vote();
+  }
+
+  private vote() {
+    this.votePayload.postId = this.post.id;
+
+    this.voteService.vote(this.votePayload).subscribe(data => {
+      this.updateVoteDetails();
+    }, error => {
+      this.toastr.error(error.error.message);
+      throwError(error);
+    })
+  }
+
+  private updateVoteDetails() {
+    this.postService.getPost(this.post.id).subscribe(data => {
+      this.post = data;
+    })
+  }
 }
